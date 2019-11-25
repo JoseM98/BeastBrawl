5c1965c126e1e82d8709386f9528754e3b1accbf
carlos <aslanadidas98@gmail.com>
2019-11-25 12:37:12 +0100

    v0.2 - Añadidas algunas físicas necesarias

--- src/systems/physics.cpp
+++ src/systems/physics.cpp
@@ -1,8 +1,34 @@
 #include "physics.h"
 
+using namespace std;
+
+#include <cmath>
+
+
 void Physics::move(CSpeed *s, CPosition *p)
 {
     p->SetX(p->GetX() + s->GetVX());
     p->SetY(p->GetY() + s->GetVY());
     p->SetZ(p->GetZ() + s->GetVZ());
+}
+
+
+float dotProduct(CPosition a, CPosition b){  //calculates dot product of a and b
+    return a.GetX() * b.GetX() + a.GetY() * b.GetY() + a.GetZ() * b.GetZ();
+}
+
+
+float magnitude(CPosition a){  //calculates magnitude of a
+    return sqrt( a.GetX() * a.GetX() + a.GetY() * a.GetY() + a.GetZ() * a.GetZ() );
+}
+
+// a=origen b=destino -- CUIDADO CON EL NEEEW
+CPosition vector3d(CPosition a, CPosition b){  //calculates magnitude of a
+    CPosition *c = new CPosition( b.GetX()-a.GetX(), b.GetY()-a.GetY(), b.GetZ()-a.GetZ() );  
+    return *c;
+}
+
+// lo dos vectores seran: uno- la direccion a la que mira el coche y otro el vecotr que uno la posActual y el punto destino
+float angle(CPosition a, CPosition b){
+    return acos(dotProduct(a,b)/(magnitude(a)*magnitude(b)));
 }
--- src/systems/physics.h
+++ src/systems/physics.h
@@ -1,6 +1,7 @@
 #ifndef __PHYSICS_H__
 #define __PHYSICS_H__
 
+
 #include "../components/speed.h"
 #include "../components/position.h"
 
@@ -11,8 +12,16 @@ public:
     ~Physics(){};
     static void move(CSpeed *s, CPosition *p);
 
+
+    float dotProduct(CPosition a, CPosition b);  //calculates dot product of a y b
+    float magnitude(CPosition a);  //calculates magnitude of a
+    CPosition vector3d(CPosition a, CPosition b);  //calculates vector3d of a y b
+    float angle(CPosition a, CPosition b);  //calculates angle of a y b
+
+
 protected:
 private:
 };
+
 
 #endif
